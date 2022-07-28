0113e9090120a5f196b98ab35cb95604a1fe559d
pgeovany <pgeovany@ic.ufrj.br>
2022-07-28 17:06:10 -0300

    questions 1 to 4 done

---
+++ queries.sql
@@ -0,0 +1,35 @@
+-- Q1
+
+SELECT COUNT(experiences."endDate") AS "currentExperiences" 
+FROM experiences;
+
+
+-- Q2
+
+SELECT educations."userId" AS id, COUNT(*) AS educations
+FROM educations
+GROUP BY educations."userId"
+ORDER BY educations DESC;
+
+
+-- Q3
+
+SELECT users.name AS writer, COUNT(*) AS "testimonialCount"
+FROM testimonials
+JOIN users
+ON users.id = testimonials."writerId"
+WHERE users.id = 435
+GROUP BY users.name
+ORDER BY "testimonialCount" DESC;
+
+
+-- Q4
+
+SELECT MAX(jobs.salary) AS "maximumSalary", roles.name AS role
+FROM jobs
+JOIN roles
+ON roles.id = jobs."roleId"
+WHERE active
+GROUP BY roles.name
+ORDER BY "maximumSalary";
+
